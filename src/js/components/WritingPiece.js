38077526d599d8ffa0727b5f8697de6832763659
Bibek <ghim.bibek@gmail.com>
2018-09-16 16:11:06 -0700

    Improve UI (esp. for mobile) for WritingPiece image

--- src/js/components/WritingPiece.js
+++ src/js/components/WritingPiece.js
@@ -4,29 +4,40 @@ import Title from './Title'
 import Text from './Text'
 import {breakpoints, shadows} from '../styles'
 
+const Image = styled.div`
+  background-image: url(${props => props.image});
+  background-size: cover;
+  background-position: center;
+`
+
 const Box = styled.a`
   background-color: white;
   box-shadow: ${shadows.default};
   border-radius: 3px;
-  padding: 10px;
 
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+
+  & > ${Image} {
+    flex-shrink: 0;
+    height: 150px;
+    width: 100%;
+  }
   
   @media (min-width: ${breakpoints.default}px) {
     flex-direction: row;
+    align-items: stretch;
+    & > ${Image} {
+      height: auto;
+      width: 30%;
+    }
   }
   text-decoration: none;
 `
 
-const Image = styled.img`
-  width: 30%;
-  flex-shrink: 0;
-`
-
 const TextInfo = styled.div`
-  padding-left: 10px;
+  padding: 10px 15px;
 `
 
 class WritingPiece extends React.Component {
@@ -34,7 +45,7 @@ class WritingPiece extends React.Component {
     const {data} = this.props
     return data ? (
       <Box href={data.link}>
-        {data.image && <Image src={data.image.src} />}
+        {data.image && <Image image={data.image.src} />}
         <TextInfo>
           <Title size={24}>{data.title}</Title>
           <Text size={18}>{data.description}</Text>
